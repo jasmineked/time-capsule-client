cc22caf666243343a3bd3ba402c4e15ec48e0d04
jasmineked <jked630@icloud.com>
2021-12-06 10:45:11 -0500

    edit: add apiUrl

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://gentle-fortress-53926.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
