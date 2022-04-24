4c21382858a2de5ac8add46abd133a7311db96e9
jasmineked <jked630@icloud.com>
2022-04-23 21:29:24 -0400

    edit: axios update call

--- src/api/entry.js
+++ src/api/entry.js
@@ -12,10 +12,42 @@ export const createEntry = (user, data) => {
   })
 }
 
-export const indexEntry = (user, data) => {
+export const indexEntry = (user) => {
   return axios({
     method: 'GET',
     url: apiUrl + '/entries',
+    headers: {
+      Authorization: `Token token=${user.token}`
+    }
+  })
+}
+
+export const updateEntry = (user, data, entryId) => {
+  return axios({
+    method: 'PATCH',
+    url: apiUrl + '/entries/' + entryId,
+    headers: {
+      'Authorization': `Token token=${user.token}`
+    },
+    // data: data
+    data: { entry: data }
+  })
+}
+
+export const showEntry = (user, entryId) => {
+  return axios({
+    method: 'GET',
+    url: apiUrl + '/entries/' + entryId,
+    headers: {
+      'Authorization': `Token token=${user.token}`
+    }
+  })
+}
+
+export const deleteEntry = (user, entryId) => {
+  return axios({
+    url: apiUrl + '/entries/' + entryId,
+    method: 'DELETE',
     headers: {
       Authorization: `Token token=${user.token}`
     }
